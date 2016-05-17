ceb4bfae83d982ae2e2f70a9ee52d437765bbbc2
Bhaskar <bhaskar.anil429@gmail.com>
2016-05-17 13:57:47 -0500

    Update SI7005.py

--- SI7005.py
+++ SI7005.py
@@ -24,12 +24,26 @@ time.sleep(0.5)
 # Checking the status, Poll RDY in status until it is low(=0)
 data = i2c.readBytes(0x40, 0x00, 3)
 print '   Read returned: ', data
-while True :
+while (data[0] & 0x01) != 0 :
 	data = i2c.readBytes(0x40, 0x00, 3)
 	print '   Read returned: ', data
 # Convert the data to 14-bits
-	ctemp = ((data[1] * 256 + data[2]) / 4.0) / 32.0 - 50.0
-	ftemp = ctemp * 1.8 + 32
+ctemp = ((data[1] * 256 + data[2]) / 4.0) / 32.0 - 50.0
+ftemp = ctemp * 1.8 + 32
+# SI7005 address, 0x40(64)
+# Select Configuration register, 0x03(03)
+#		0x01(01)	Relative Humidity, Fast mode enable, Heater Off
+val = i2c.writeBytes(0x40, 0x03, 0x01)
+
+time.sleep(0.5)
+
+# SI7005 address, 0x40(64)
+# Read data back from 0x00(00), 3 bytes
+# Status register, humidity MSB, humidity LSB
+# Checking the status, Poll RDY in status until it is low(=0)
+data = i2c.readBytes(0x40, 0x00, 3)
+while (data[0] & 0x01) != 0 :
+	data = i2c.readBytes(0x40, 0x00, 3)
 	humidity = ((data[1] * 256 + data[2]) / 16.0) / 16.0 - 24.0
 
 # Output data to screen
