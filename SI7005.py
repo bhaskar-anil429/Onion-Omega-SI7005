3460a7b5100af7daa65c6232a383606e604d1af8
Bhaskar <bhaskar.anil429@gmail.com>
2016-05-17 13:09:51 -0500

    Update SI7005.py

--- SI7005.py
+++ SI7005.py
@@ -1,13 +1,20 @@
-import smbus
+from OmegaExpansion import onionI2C
 import time
+import sys
 
-# Get I2C bus
-bus = smbus.SMBus(1)
+print 'Starting: onionI2C module testing...'
+
+i2c 	= onionI2C.OnionI2C(0)
+
+# set the verbosity
+i2c.setVerbosity(1)
 
 # SI7005 address, 0x40(64)
 # Select Configuration register, 0x03(03)
 #		0x11(11)	Temperature, Fast mode enable, Heater Off
-bus.write_byte_data(0x40, 0x03, 0x11)
+value = [0x11]
+val 	= i2c.writeBytes(0x40, 0x03, value)
+print '   writeBytes returned: ', val
 
 time.sleep(0.5)
 
@@ -15,10 +22,11 @@ time.sleep(0.5)
 # Read data back from 0x00(00), 3 bytes
 # Status register, ctemp MSB, ctemp LSB
 # Checking the status, Poll RDY in status until it is low(=0)
-data = bus.read_i2c_block_data(0x40, 0x00, 3)
+data = i2c.readBytes(0x40, 0x00, 3)
+print '   Read returned: ', data
 while (data[0] & 0x01) != 0 :
-	data = bus.read_i2c_block_data(0x40, 0x00, 3)
-
+	data = i2c.readBytes(0x40, 0x00, 3)
+	print '   Read returned: ', data
 # Convert the data to 14-bits
 ctemp = ((data[1] * 256 + data[2]) / 4.0) / 32.0 - 50.0
 ftemp = ctemp * 1.8 + 32
@@ -26,18 +34,19 @@ ftemp = ctemp * 1.8 + 32
 # SI7005 address, 0x40(64)
 # Select Configuration register, 0x03(03)
 #		0x01(01)	Relative Humidity, Fast mode enable, Heater Off
-bus.write_byte_data(0x40, 0x03, 0x01)
-
+value = [0x01]
+val = i2c.writeBytes(0x40, 0x03, value)
+print '   writeBytes returned: ', val
 time.sleep(0.5)
 
 # SI7005 address, 0x40(64)
 # Read data back from 0x00(00), 3 bytes
 # Status register, humidity MSB, humidity LSB
 # Checking the status, Poll RDY in status until it is low(=0)
-data = bus.read_i2c_block_data(0x40, 0x00, 3)
+data = i2c.readBytes(0x40, 0x00, 3)
 while (data[0] & 0x01) != 0 :
-	data = bus.read_i2c_block_data(0x40, 0x00, 3)
-
+	data = i2c.readBytes(0x40, 0x00, 3)
+	print '   Read returned: ', data
 # Convert the data to 12-bits
 humidity = ((data[1] * 256 + data[2]) / 16.0) / 16.0 - 24.0
 
