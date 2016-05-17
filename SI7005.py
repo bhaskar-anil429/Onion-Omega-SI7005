11ee46c782f0e3ba917e56dbc57f50df55f3e9d3
Bhaskar <bhaskar.anil429@gmail.com>
2016-05-17 13:45:56 -0500

    Update SI7005.py

--- SI7005.py
+++ SI7005.py
@@ -24,33 +24,16 @@ time.sleep(0.5)
 # Checking the status, Poll RDY in status until it is low(=0)
 data = i2c.readBytes(0x40, 0x00, 3)
 print '   Read returned: ', data
-while (data[0] & 0x01) != 0 :
+while True :
 	data = i2c.readBytes(0x40, 0x00, 3)
 	print '   Read returned: ', data
 # Convert the data to 14-bits
-ctemp = ((data[1] * 256 + data[2]) / 4.0) / 32.0 - 50.0
-ftemp = ctemp * 1.8 + 32
-
-# SI7005 address, 0x40(64)
-# Select Configuration register, 0x03(03)
-#		0x01(01)	Relative Humidity, Fast mode enable, Heater Off
-value = [0x01]
-val = i2c.writeBytes(0x40, 0x03, value)
-print '   writeBytes returned: ', val
-time.sleep(0.5)
-
-# SI7005 address, 0x40(64)
-# Read data back from 0x00(00), 3 bytes
-# Status register, humidity MSB, humidity LSB
-# Checking the status, Poll RDY in status until it is low(=0)
-data = i2c.readBytes(0x40, 0x00, 3)
-while (data[0] & 0x01) != 0 :
-	data = i2c.readBytes(0x40, 0x00, 3)
-	print '   Read returned: ', data
-# Convert the data to 12-bits
-humidity = ((data[1] * 256 + data[2]) / 16.0) / 16.0 - 24.0
+	ctemp = ((data[1] * 256 + data[2]) / 4.0) / 32.0 - 50.0
+	ftemp = ctemp * 1.8 + 32
+	humidity = ((data[1] * 256 + data[2]) / 16.0) / 16.0 - 24.0
 
 # Output data to screen
-print "Relative Humidity : %.2f %%" %humidity
-print "Temperature in Celsius : %.2f C" %ctemp
-print "Temperature in Fahrenheit : %.2f F" %ftemp
+	print "Relative Humidity : %.2f %%" %humidity	
+	print "Temperature in Celsius : %.2f C" %ctemp
+	print "Temperature in Fahrenheit : %.2f F" %ftemp
+	time.sleep(3)
